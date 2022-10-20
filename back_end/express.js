154a9774739e3c4b22fe1acdde5594a693d79b45
nameless0724 <ijebeja@gmail.com>
2022-10-20 20:36:19 +0800

    user profile API (express.js) - 20 October 2022

--- back_end/express.js
+++ back_end/express.js
@@ -1,35 +1,167 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import { connectDatabase } from "./pool.js";
+import bodyParser from "body-parser";
+import express from "express";
+import bcrypt from "bcryptjs";
+import { v4 as uuidv4 } from "uuid"; //universal unique identifier
+import { generateJwt } from "./jwtGenerator.js";
+import { auth } from "./auth.js";
+
+const pool = connectDatabase()
 const app = express()
-const port = 3000
-app.use(bodyParser.json())
-app.use( bodyParser.urlencoded({ extended: true, }) )
+const PORT = 8000
 
-const Pool = require('pg').Pool
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database:'api',
-    password: '24-J1u9l8y9',
-    port: 5432,
-})
-
-app.get('/', (req, res) => {
-    res.json(
-        { info: 'Node.js, Express, and Postgre API' }
-    )
-})
-
-app.get("/login", async (req, res) => {
-    try {
-        const user = await pool.query('SELECT * FROM public.user ORDER BY id ASC');
-        res.json(user.rows)
-    } catch (error) {
-        console.log(error)
+pool.connect((err) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        app.listen(PORT, () => {
+            console.log(`Server has started on http://localhost:${PORT}`)
+        })
     }
 })
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+app.post('/signup', async (req, res) => {
+    try {
+        //take the email and password from the req.body
+        const {
+            email,
+            password
+        } = req.body
+
+        //check if the user is already existing 
+        const user = await pool.query(`SELECT * FROM public.user WHERE email = $1`, [email]) //$ for placement for array
+
+        if (user.rows.length > 0) {
+            res.status(401).send("User already exists!")
+        }
+
+        //setup bcrypt for password hashing
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(password, salt);
+
+        //add the new user into the database
+        //generate the uuid using the uuidv4() function
+        const newUser = await pool.query(`
+        INSERT INTO public.user (id, email, password) VALUES ($1, $2, $3) RETURNING * 
+        `, [uuidv4(), email, bcryptPassword]) //table database
+
+        //generate and return the JWT token
+        const token = generateJwt(newUser.rows[0])
+
+        res.json({
+            token
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try{
+        //take the email and password from the req.body
+        const {
+            email,
+            password
+        } = req.body;
+
+        //check if the user is not existing
+        const user = await pool.query(`SELECT * FROM public.user WHERE email = $1`, [email])
+        
+        console.log(user.rows.length)
+        if (user.rows.length == 0) {
+            res.status(401).send("User does not exists!")
+        }
+        
+        //check if  the password matches using bcrypt
+        const validPassword = await bcrypt.compare(password, user.rows[0].password)
+        if (!validPassword) {
+            return res.status(401).json("Password or Email is incorrect!")
+        }
+       
+        //generate and return the JWT
+        const token = generateJwt(user.rows[0])
+        res.json({
+            token
+        })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({
+            msg: "Unauthenticated"
+        })
+    }
+})
+
+//provide the auth middleware
+app.get('/verify', auth, async (req, res) => {
+    try{
+        res.json(req.user)
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send({
+            msg: "Unautheticated"
+        });
+    }
+})
+
+//for user profile
+app.post('/newprofile', async (req, res) => {
+    try {
+        const {
+            user,
+            sss,
+            philhealth,
+            pagibig,
+            tax
+        } = req.body
+        
+        const profile = await pool.query(`SELECT * FROM public.profile WHERE user = $1`, [user])
+
+        if (profile.rows.length > 0) {
+            res.status(401).send("Profile already exists!")
+        }
+
+        const newProfile = await pool.query(`
+        INSERT INTO public.profile (id, user, sss, philhealth, pagibig, tax) VALUE ($1, $2, $3, $4, $5, $6) RETURNING *
+        `, [uuidv4(), user, sss, philhealth, pagibig, tax])
+
+        const token = generateJwt(newProfile.rows[0])
+
+        res.json({
+            token
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+})
+
+app.post('/profile', async (req, res) => {
+    try{
+        const {
+            user
+        } = req.body;
+
+        const profile = await pool.query(`SELECT * FROM public.profile WHERE user = $1`, [user])
+
+        console.log(profile.rows.length)
+        if (profile.rows.length == 0) {
+            res.status(401).send("Profile not found!")
+        }
+
+        const token = generateJwt(profile.rows[0])
+        res.json({
+            token
+        })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({
+            msg: "Unauthenticated"
+        })
+    }
 })

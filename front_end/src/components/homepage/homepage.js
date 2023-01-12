6b07848f0dae660de4ca24c250a1bba8d8b4dc5b
nameless0724 <ijebeja@gmail.com>
2023-01-12 21:47:54 +0800

    UI for homepage

--- front_end/src/components/homepage/homepage.js
+++ front_end/src/components/homepage/homepage.js
@@ -10,23 +10,25 @@ function HomePage() {
                 <div>
                     <img className="md:auto" src={main} alt="main" width="500" height="512"></img>
                 </div>
-                <div>
+                <div className="mt-10">
                     <h1 className="text-5xl text-left font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-lime-400 to-green-500">EZPS</h1>
-                </div>
-                <div>
                     <p className="text-3xl text-white">Easy Pay Slip access anytime.<br /> Anywhere.<br /> At your most convient.</p>
                 </div>
             </div>
-            <div className="container grid grid-cols-2  mx-auto">
+            <div className="container grid grid-flow-col-dense mx-auto">
                 <div className="max-w-sm p-6 bg-gray-700 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <img className="display:block" src={signup} alt="signup"></img>
                     <h3 className="text-3xl text-center text-white">Haven't registered yet?</h3>
-                    <button className="bg-lime-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded-full"><a href="/signup">SIGN UP</a></button>
+                    <div className="flex flex-col justify-center items-center md:auto">
+                        <button className="bg-lime-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded-full"><a href="/signup">SIGN UP</a></button>
+                    </div>
                 </div>
                 <div className="max-w-sm p-6 bg-gray-700 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <img className="display:block" src={login} alt="login"></img>
                     <h3 className="text-3xl text-center text-white">Welcome back!</h3>
-                    <button className="bg-lime-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded-full"><a href="/login">LOG IN</a></button>
+                    <div className="flex flex-col justify-center items-center md:auto">
+                        <button className="bg-lime-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded-full"><a href="/login">LOG IN</a></button>
+                    </div>
                 </div>
             </div>
         </div>

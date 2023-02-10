78295e600dcdf65d468aabad03a271eb11cb832b
nameless0724 <ijebeja@gmail.com>
2023-02-10 21:42:38 +0800

    update in backend

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/homepage/homepage';
-import LoginPage from './components/login/loginPage';
-import PayslipPage from './components/payslip/payslipPage';
-import SignupPage from './components/signup/signupPage';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-
-        <Route path="payslip" element={<PayslipPage />} />
       </Routes>
     </BrowserRouter> 
   ) 
